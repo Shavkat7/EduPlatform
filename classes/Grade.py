b7dbd9f9a861ba2115749235563429f0716dbeba
Shavkat7 <shavkat.official007@gmail.com>
2025-06-14 15:58:42 +0500

    grade

--- classes/Grade.py
+++ classes/Grade.py
@@ -1,20 +1,22 @@
-
+import datetime
 
 class Grade:
-    id: int
     student_id: int
     subject: str
     value: int # from 1 to 5
     date: str # ISO format
     teacher_id: int
 
-    def __init__(self, id: int, student_id: int, subject: str, value: int, date: str, teacher_id: int):
-        self.id = id
+    def __init__(self, student_id, subject, value, teacher_id, comment=""):
+        self.id = Grade._id_counter
+        Grade._id_counter += 1
+
         self.student_id = student_id
         self.subject = subject
         self.value = value
-        self.date = date
+        self.date = datetime.datetime.now().isoformat()
         self.teacher_id = teacher_id
+        self.comment = comment
 
     def update_grade(self, value: int):
         if 1 <= value <= 5:
@@ -30,5 +32,6 @@ class Grade:
             "subject": self.subject,
             "value": self.value,
             "date": self.date,
-            "teacher_id": self.teacher_id
+            "teacher_id": self.teacher_id,
+            "comment": self.comment
         }
