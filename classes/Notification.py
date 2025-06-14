8f17cae0c59f59f270d141a06e84d2a6858d0bc5
Shavkat7 <shavkat.official007@gmail.com>
2025-06-14 16:03:59 +0500

    notification

--- classes/Notification.py
+++ classes/Notification.py
@@ -1,29 +1,35 @@
-
-
+import datetime
+import All_Users_Data
 
 class Notification:
 
     # Attributes:
-    id: int
     message: str
     recipient_id: int
     created_at: str # ISO format
 
 
-    def __init__(self, id: int, message: str, recipient_id: int, created_at: str):
-        self.id = id
+    _id_counter = 1
+
+    def __init__(self, message, recipient_id, priority="Normal"):
+        self.id = Notification._id_counter
+        Notification._id_counter += 1
+
         self.message = message
         self.recipient_id = recipient_id
-        self.created_at = created_at
+        self.created_at = datetime.datetime.now().isoformat()
+        self.is_read = False
+        self.priority = priority
 
+    def send(self):
+        recipient = All_Users_Data.users.get(self.recipient_id)
+        if recipient:
+            recipient.add_notification(self.message)
+            print(f"Notification sent to {recipient._full_name}.")
+        else:
+            print("Recipient not found.")
 
-    def send(self, message, recipient_id):
-        # Logic to send the notification
-        print(f"Notification sent to {self.recipient_id}: {self.message} at {self.created_at}")
-        return True
     
     def mark_as_read(self):
-        # Logic to mark the notification as read
-        print(f"Notification {self.id} marked as read.")
-        return True
+        self.is_read = True
     
